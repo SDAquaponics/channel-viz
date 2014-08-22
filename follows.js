f159859a73939fe87a50093083b693aec940788c
seemantadutta <seemanta@gmail.com>
2014-08-22 00:21:37 -0700

    Update follows.js
    Customizing the javascript - first step

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'UGEfz8qRCKhVhvT8hjWpd5drnwWmZPLfQ25DG1mJI0ieDAKc', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [584440607], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Henry\'s Aquaponics', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
